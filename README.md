ab3cc8b01aa956a9b79cbee05f9ca24b1a029108
Joell Jones <joelljones86@gmail.com>
2018-03-20 18:54:55 -0400

    implement functionality

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Pixel Art Maker Project
+
+Users should be able to:
+
+1. Dynamically set the size of the table as an N by M grid.
+
+2. Choose a color.
+
+3. Click a cell in the grid to fill that cell with the chosen color.
+
+4. The primary task is to implement the makeGrid() function, that dynamically creates a grid that the user can interact with.
+
+<br />
+
+![Screenshot](screen-shot.png)
--- designs.js
+++ designs.js
@@ -1,10 +1,36 @@
-// Select color input
-// Select size input
+$(function() {
 
-// When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+  const $form = $('#sizePicker')      // Select size input
+  const $color = $('#colorPicker');   // Select color input
+  const $canvas = $('#pixelCanvas');  // Select canvas
 
-// Your code goes here!
+  // When size is submitted by the user, call makeGrid()
+  $form.submit((e) => {
+    e.preventDefault();
+    $canvas.empty();
+    makeGrid();
+  });
 
-}
+  // Create grid
+  function makeGrid() {
+    let $height = $('#inputHeight').val();
+    let $width = $('#inputWidth').val();
+
+    for (let x = 1; x <= $height; x++) {
+      $canvas.append('<tr></tr>');
+      for (let y = 1; y <= $width; y++) {
+        $('tr:last').append('<td class="cells"></td>');
+        $('td').attr('class','cells');
+      }
+    }
+  };
+
+  // Each cell should have an event listener
+  // that sets the background color of the cell to the selected color
+  $canvas.click('.cells', (e) => {
+    e.preventDefault();
+    $(e.target).css('background-color', $color.val());
+  });
+
+});
